94c82a9cd1bf1e9b3bc086cb30b3b61060e43a7c
YuuyaOkatani <yuuyaokatani@gmail.com>
2025-06-09 04:10:32 -0300

    :sparkles: feat: Added conditions to program recognize arduino data with keywords

--- arduino_virtual.py
+++ arduino_virtual.py
@@ -2,6 +2,7 @@ import serial
 import time
 import threading
 import pygame
+import random
 
 VIRTUAL_ARDUINO_PORT = "COM6"
 BAUD_RATE = 9600
@@ -113,30 +114,183 @@ DICAS = [
 print(f"Iniciando conexão na porta {VIRTUAL_ARDUINO_PORT}...")
 
 
-try:
-    ser = serial.Serial(VIRTUAL_ARDUINO_PORT, BAUD_RATE, timeout=0.1)
-    time.sleep(1)
-    print(f"Conexão bem-sucedida na porta {VIRTUAL_ARDUINO_PORT}!")
+class ArduinoVirtual:
+    def __init__(self, port, baudrate):
+        self._vidas = 6
+        self._erros = 0
+        self._port = port
+        self._baudrate = baudrate
 
-    while True:
-        if ser.in_waiting > 0:
-            received_data = ser.readline().decode("utf-8").strip()
-            print(f"[RX] Recebido do Comptador: {received_data}")
+        # Pega um número aleatório.
+        self._numero_aleatorio = random.randint(1, 50)
 
-            if received_data.strip() == "":
-                response = "Digite algo!"
+        # Cria uma variável e seleciona uma palavra aleatória através de um index.
+        self._mensagem_segredo_real = PALAVRAS[self._numero_aleatorio]
+
+        # Mascara a palavra aleatóriamente selecionada.
+        self._dados_segredo = self.mascarar_palavra(self._mensagem_segredo_real)
+
+        # Para verificar se houve alguma mudança na palavra secreta.
+        self._chance_mudanca = False
+
+        # Iniciar arduino quando a classe for instanciada.
+        self.Iniciar_arduino()
+
+    def mascarar_palavra(self, mensagem):
+        palavra_mascarada = []
+        for letra in mensagem:
+            if letra.isalpha():
+                palavra_mascarada.append("_")
+            elif letra == " ":
+                palavra_mascarada.append(" ")
+            elif letra == "-":
+                palavra_mascarada.append("-")
+        return "".join(palavra_mascarada)
+
+    # método que vai adicionar as letras. (palavra já modificada, palavra selecionada da lista letra selecionada)
+    def adicionar_palavra(self, letra_selecionada):
+        palavra_mascarada = []
+
+        # Para cada letra da palavra já modificada,
+        # com letras, se tiver uma letra semelhante a selecionada,
+        # não será adicionada novamente.
+
+        # Para cada letra da palavra que foi selecionado no sorteio, é inserido nova letra.
+        # Ex:
+        # Palavra: ARRAIA
+        # palavra já modificada: _RR_I_
+        for i in range(len(self._mensagem_segredo_real)):
+            if self._dados_segredo[i].isalpha():
+                palavra_mascarada.append(self._dados_segredo[i])
+            elif self._dados_segredo[i] == "-":
+                palavra_mascarada.append("-")
+            elif self._mensagem_segredo_real[i] == letra_selecionada:
+                palavra_mascarada.append(self._mensagem_segredo_real[i])
             else:
-                response = "Dados recebidos: " + received_data
-            ser.write(response.encode("utf-8"))
-            print(f"[TX] Enviado ao PC: {response.strip()}")
-        time.sleep(0.05)
+                palavra_mascarada.append("_")
 
-except serial.SerialException as e:
-    print(f"Algo de errado na porta emulador: {e}")
+        return "".join(palavra_mascarada)
 
-except KeyboardInterrupt:
-    print("Simulador encerrado")
-finally:
-    if "ser" in locals() and ser.is_open:
-        ser.close()
-        print("Porta do simulador fechado")
+    def Iniciar_arduino(self):
+        # Para acionar o pySerial. Enquanto for verdadeiro, o pySerial contiunará funcionando.
+        self._on_active = True
+        try:
+            ser = serial.Serial(self._port, self._baudrate, timeout=0.1)
+            time.sleep(1)
+            print(f"Conexão bem-sucedida na porta {self._port}!")
+
+            pygame.init()
+            pygame.mixer.init()
+
+            # Colocando uma variável para verificar se há diferença entre a palavra anterior e o atual
+            # Ex: __ATO == ____ (prato)?
+
+            mensagem_dica_teste = "Dica: " + DICAS[self._numero_aleatorio] + "\n"
+
+            mensagem_dados_segredo = "Segredo: " + self._dados_segredo + "\n"
+            mensagem_vidas = "Vidas: " + str(self._vidas) + "\n"
+
+            mensagem_erros = f"Erros: {str(self._erros)}" + "\n"
+
+            message_reset = "resetgame" + "\n"
+            ser.write(mensagem_dica_teste.encode("utf-8"))
+            ser.write(mensagem_dados_segredo.encode("utf-8"))
+            ser.write(mensagem_vidas.encode("utf-8"))
+            ser.write(mensagem_erros.encode("utf-8"))
+
+            ser.write(message_reset.encode("utf-8"))
+            while self._on_active:
+
+                if ser.in_waiting > 0:
+                    received_data = ser.readline().decode("utf-8").strip()
+                    print(f"[RX] Recebido do Comptador: {received_data}")
+
+                    if (
+                        len(received_data.strip()) > 1
+                        or len(received_data.strip()) < 1
+                        or not received_data.strip().isalpha()
+                    ):
+                        print("Digite um caractere válido")
+                        self._chance_mudanca = True
+                        mensagem_digite_um_caractere_valido = (
+                            "digiteumcaraterevalido" + "\n"
+                        )
+                        ser.write(mensagem_digite_um_caractere_valido.encode("utf-8"))
+                    # Para cada letra da palavra real, verfica se a letra existe
+                    elif len(received_data.strip()) == 1:
+                        if received_data.strip().upper() in self._dados_segredo:
+                            print("Letra já selecionada. Selecione outros.")
+                            mensagem_letra_ja_selecionada = (
+                                "Letra_ja_selecionada:"
+                                + received_data.strip().upper()
+                                + "\n"
+                            )
+                            ser.write(mensagem_letra_ja_selecionada.encode("utf-8"))
+                            self._chance_mudanca = True
+                        else:
+                            for letter in self._mensagem_segredo_real:
+                                if letter == received_data.upper().strip():
+                                    # Modifica a palavra caso letra exista
+                                    self._dados_segredo = self.adicionar_palavra(letter)
+                                    self._chance_mudanca = True
+                                # Verifica se houve alguma mudança na palavra.
+                    if self._chance_mudanca == False:
+                        self._vidas -= 1
+                        self._erros += 1
+
+                        self._chance_mudanca = False
+                    else:
+                        self._chance_mudanca = False
+                    # Envia todos os dados para serem filtrados pelo programa python
+                    mensagem_vidas = f"Vidas: {str(self._vidas)}" + "\n"
+                    mensagm_erros = "Erros: " + str(self._erros) + "\n"
+                    mensagem_dados_segredo = "Segredo: " + self._dados_segredo + "\n"
+
+                    print(f"Quantidade de vidas: {str(self._vidas)}")
+                    print(f"Quantidade de erros: {str(self._erros)}")
+                    print(f"Palavra: {mensagem_dados_segredo}")
+
+                    ser.write(mensagem_vidas.encode("utf-8"))
+                    ser.write(mensagm_erros.encode("utf-8"))
+                    ser.write(
+                        mensagem_dados_segredo.encode("utf-8")
+                    )  # Palavra com mascara (e com letras corretas)
+                    if self._vidas == 0:
+                        self._on_active = False
+                        print(f"GAME OVER")
+
+                        self._mensagem_game_over = "gameover" + "\n"
+                        ser.write(self._mensagem_game_over.encode("utf-8"))
+                        game_over_audio = "audio/game_over.wav"
+
+                        pygame.mixer.music.load(game_over_audio)
+                        pygame.mixer.music.play()
+                        pygame.time.wait(8000)
+
+                    time.sleep(1)
+
+                    if "_" not in self._dados_segredo:
+                        self._on_active = False
+                        print(f"VOCÊ VENCEU!")
+
+                        self._mensagem_level_complete = "levelcomplete" + "\n"
+                        ser.write(self._mensagem_level_complete.encode("utf-8"))
+                        level_complete = "audio/level_complete.wav"
+
+                        pygame.mixer.music.load(level_complete)
+                        pygame.mixer.music.play()
+                        pygame.time.wait(8000)
+
+        except serial.SerialException as e:
+            print(f"Algo de errado na porta emulador: {e}")
+
+        except KeyboardInterrupt:
+            print("Simulador encerrado")
+        finally:
+            if "ser" in locals() and ser.is_open:
+                ser.close()
+                print("Porta do simulador fechado")
+
+
+if __name__ == "__main__":
+    p = ArduinoVirtual(VIRTUAL_ARDUINO_PORT, BAUD_RATE)
--- homepage.py
+++ homepage.py
@@ -4,8 +4,9 @@ import pygame
 import time
 import serial
 import customtkinter as ctk
-
+import threading
 from pyfirmata import Arduino, util
+import ast
 
 root = ctk.CTk()
 root.title("Jogo da forca")
@@ -13,45 +14,149 @@ root.geometry("1000x600")
 ctk.set_appearance_mode("System")
 
 ARDUINO_PORT = "COM7"
-conexao: None
-# LED_PIN = 13
-# ANALOG_PIN = 0
+conexao = None
 
 
 class ArduinoComunicator:
 
-    # Método para inicializar tudo
+    # Método para inicializar tudo (Válido lembrar que "self" serve para criar qualquer variável.)
     def __init__(self, port):
         self._port = port
         self._conexao = None
+        self._is_reading_true = True
+        self._quantidade_vidas = 6
+        self._erros = 0
+        self._dica = ""
+        self._segredo = ""
+        self._letra_ja_digitada = False
+        self._is_game_over = False
+        self._is_level_complete = False
+        self._is_caracter_valid_false = False
+
         self.Stabilize_Connection()
 
+    # Método para realizar uma conexão com a porta de arduino.
     def Stabilize_Connection(self):
 
         print(f"--Testando conexão com o {self._port}....--")
         try:
             self._conexao = serial.Serial(self._port, 9600, timeout=0.1)
+            time.sleep(2)
+            self._conexao.reset_input_buffer()
             print(f"Conexão feita com sucesso na porta {self._port}!")
+            read_thread = threading.Thread(
+                target=self.Receber_dados_continuamente, daemon=True
+            )
+            read_thread.start()
+
         except serial.SerialException as e:
             print(f"Algo deu muito errado {e}")
             self._conexao = None
 
+    # Realiza conexão com threading para não consumir totalmente a memória.
+    def Receber_dados_continuamente(self):
+        while self._is_reading_true:
+            try:
+                if self._conexao and self._conexao.in_waiting > 0:
+                    received_data = self._conexao.readline().decode("utf-8").strip()
+                    self._is_game_over = False
+                    self._is_level_complete = False
+                    # Condições necessárias para interpretar os dados do arduino.
+                    # Aqui pode realizar mudanças de variáveis para qualquer
+                    # palavra-chave que seu ARDUINO através da outra porta.
+
+                    # Ex: se tiver palavra "dica" no Serial.println("dica"),
+                    # então vai acionar o que está dentro da condição.
+                    if "dica" in received_data.lower().strip():
+                        print(f"Dica: { received_data}")
+                        self._dica = received_data
+
+                        time.sleep(0.01)
+
+                    if "segredo" in received_data.lower().strip():
+                        print(f"palavra sorteada: {received_data}")
+                        self._segredo = (
+                            received_data.strip().replace("Segredo:", "").strip()
+                        )
+                        print(f"Dados formatados do segredo: {self._segredo}")
+
+                        time.sleep(0.01)
+
+                    if "erros" in received_data.lower().strip():
+                        print(f"Quantidade de erros {received_data}")
+                        self._erros = int(
+                            received_data.strip().replace("Erros:", "").strip()
+                        )
+                        print(f"Dados formatados erros: {self._erros}")
+                        time.sleep(0.01)
+                    if "vidas" in received_data.lower().strip():
+                        print(f"Quantidade de vidas: {received_data}")
+                        self._quantidade_vidas = int(
+                            received_data.strip().replace("Vidas:", "").strip()
+                        )
+                        print(f"Dados formatados vidas: {self._quantidade_vidas}")
+                        time.sleep(0.01)
+                    if "letra_ja_selecionada" in received_data.lower().strip():
+                        print(f"Letra já selecionada. Digite outro. {received_data}")
+                        self._letra_ja_digitada = True
+                        time.sleep(0.01)
+
+                    if "gameover" in received_data.lower().strip():
+                        print(f"GAME OVER")
+                        self._is_game_over = True
+
+                    if "levelcomplete" in received_data.lower().strip():
+                        print(f"LEVEL COMPLETE")
+                        self._is_level_complete = True
+
+                    if "digiteumcaraterevalido" in received_data.lower().strip():
+                        print("Digite um caractere válido")
+                        self._is_caracter_valid_false = True
+
+                    if "resetgame" in received_data.lower().strip():
+
+                        print("Jogo resetado.")
+
+                    time.sleep(0.02)
+
+            except serial.SerialException as e:
+                print(f"Algo deu errado no recebimento contínuo de dados: {e}")
+                self._is_reading_true = False
+                if self._conexao:
+                    self._conexao.close()
+                self._conexao = None
+                print("Conexão foi desligada por causa de um erro.")
+                break
+            except Exception as e:
+                print(f"Algo deu errado no recebimento contínuo de dados: {e}")
+                self._is_reading_true = False
+                break
+
+    # Método que enviar dados para outra porta do par COM
     def Requisitar_Arduino_Dados(self, data):
         try:
             self._data = data + "\n"
             self._conexao.write(self._data.encode("utf-8"))
             print("Mensagem enviada.")
+            self._letra_ja_digitada = False
+            self._is_caracter_valid_false = False
+            self._is_game_over = False
+            self._is_level_complete = False
         except serial.SerialException as e:
             print(f"Algo deu muito errado: {e}")
 
-    def Resposta_Arduino_Dados(self):
-        try:
-            self._data = self._conexao.readline().decode("utf-8").strip()
-            print(f"Dados recebidos do arduino: {self._data}")
-            return self._data
-
-        except serial.SerialException as e:
-            print(f"Erro ao receber os dados necessários: {e}")
+    # Envia os estados modificados pela resposta das condições do arduino.
+    def Reposta_arduino_dados(self):
+        return (
+            self._quantidade_vidas,
+            self._erros,
+            self._dica,
+            self._segredo,
+            self._letra_ja_digitada,
+            self._is_game_over,
+            self._is_level_complete,
+            self._is_caracter_valid_false,
+        )
 
 
 # Válido lembrar que existe o "Master", que serve como "pano de fundo".
@@ -61,9 +166,14 @@ class GUI:
         self._master = master
         # Padrão decorator para usar outra classe e usa-la.
         self._initializer = ArduinoComunicator(arduino_port)
-
+        self._escutando = False
         # Primeiro frame
-        self._frame1 = ctk.CTkFrame(master=self._master, height=600, width=1000)
+        self._frame1 = ctk.CTkFrame(master=self._master, height=800, width=1000)
+
+        self._vidas = 6
+        self._erros = 0
+        self._dicas = ""
+        self._segredo = ""
 
         # Cabeçalho
         self._label1 = ctk.CTkLabel(
@@ -73,36 +183,121 @@ class GUI:
         self._label2 = ctk.CTkLabel(
             master=self._frame1,
             text="",
+            font=("Arial", 25, "bold"),
         )
+
+        self._label3 = ctk.CTkLabel(
+            master=self._frame1,
+            text="",
+            font=("Arial", 25, "bold"),
+        )
+        self._label4 = ctk.CTkLabel(
+            master=self._frame1,
+            text="",
+            font=("Arial", 25, "bold"),
+        )
+        self._label5 = ctk.CTkLabel(
+            master=self._frame1,
+            text="",
+            font=("Arial", 25, "bold"),
+        )
+
         # Outro cabeçalho
         self._label_saida = ctk.CTkLabel(
-            master=self._frame1, text="", font=("Roboto", 14, "italic")
+            master=self._frame1,
+            text="",
+            font=("Arial", 25, "bold"),
         )
 
         self._input1 = ctk.CTkEntry(master=self._frame1, height=20, width=200)
         self._button_enter = ctk.CTkButton(
             master=self._frame1,
             width=200,
-            height=20,
+            height=50,
+            bg_color="#34eb58",
             text="Verficar",
             command=self.enviarDadoParaArduino,
         )
 
-        self._frame1.pack(pady=20)
+        # Para enpacotar tudo e mostrar na interface.
+        self._frame1.pack(pady=50, fill="both", expand=True)
         self._label1.pack(pady=10)
         self._label2.pack(pady=10)
+        self._label3.pack(pady=10)
         self._input1.pack(pady=10)
+        self._label4.pack(pady=10)
+        self._label5.pack(pady=10)
         self._button_enter.pack(pady=10)
         self._label_saida.pack(pady=10)
 
+    # Enviar e receber os dados do arduino.
     def enviarDadoParaArduino(self):
         message = self._input1.get()
         self._initializer.Requisitar_Arduino_Dados(message)
-        self._label_saida.configure(text=message)
-        self._suco = self._initializer.Resposta_Arduino_Dados()
-        print("dados:" + self._suco)
+        self._label_saida.configure(text=f"letra digitada: {message}")
+
+    # Método para conseguir resposta do arduino de forma cotínua, atualizando a cada segundo.
+    def escutar_dados_connector_arduino(self):
+        while self._escutando:
+            (
+                self._vidas,
+                self._erros,
+                self._dicas,
+                self._segredo,
+                self._letra_ja_selecionada,
+                self._is_game_over,
+                self._is_level_complete,
+                self._is_caractere_valid_false,
+            ) = self._initializer.Reposta_arduino_dados()
+            print(f"Vidas: {self._vidas} ")
+            print(f"Erros: {self._erros}")
+            print(f"Dicas: {self._dicas}")
+            print(f"Palavra: {self._segredo}")
+            print(f"Verificação se ja foi selecionada: {self._letra_ja_selecionada}")
+            print(
+                f"Verificação se o jogo foi resetado: LEVEL COMPLETE? {self._is_level_complete}"
+            )
+            print(f"Verificar se o jogo já resetou: GAME OVER? {self._is_game_over}")
+
+            self._label2.configure(text=self._dicas)
+            self._label3.configure(text=" ".join(self._segredo))
+            self._label4.configure(text=f"Vidas: {self._vidas}")
+            self._label5.configure(text=f"Erros: {self._erros}")
+            if self._letra_ja_selecionada == True:
+                self._label_saida.configure(text="Letra já selecionada. Escolha outro.")
+
+            if self._is_game_over == True:
+                self._label_saida.configure(text="GAME OVER")
+                time.sleep(8)
+                self._label_saida.configure(text="")
+
+            if self._is_level_complete == True:
+                self._label_saida.configure(text="você venceu!")
+                time.sleep(8)
+                self._label_saida.configure(text="")
+
+            if self._is_caractere_valid_false == True:
+                self._label_saida.configure(text="Selecione caractere válido")
+
+            time.sleep(0.5)
+
+    # Método para executar a escuta através da Multithreading.
+    def executar_escuta(self):
+        self._escutando = True
+        self._thread = threading.Thread(
+            target=self.escutar_dados_connector_arduino, daemon=True
+        )
+        self._thread.start()
+        print("Escuta em segundo plano realizado com sucesso.")
+
+    # Desligar a escuta.
+    def parar_escuta(self):
+        self._escutando = False
+        self._thread.join()
+        print("Escuta desligada com sucesso.")
 
 
 if __name__ == "__main__":
     p = GUI(master=root, arduino_port=ARDUINO_PORT)
+    p.executar_escuta()
     root.mainloop()
